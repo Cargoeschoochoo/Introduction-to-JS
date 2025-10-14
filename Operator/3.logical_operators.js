4e9b29887bd4aef554a6a8f14479b638ddf1687f
Cargoeschoochoo <150305988+Cargoeschoochoo@users.noreply.github.com>
2025-10-14 22:00:18 +0000

    Added files

--- Operator/3.logical_operators.js
+++ Operator/3.logical_operators.js
@@ -3,5 +3,13 @@
  * And: &&
  * Not: !
  * Nullish coalescing operator: ??
+ * provides a default value if the operand before the ?? is false
+ * return the right hadn side value of the left-hand value is null or undefined
  */
+
+let user = null;
+const username = user ?? 'default user';
+const dummyUsername = (user !== null && user !== undefined) ? user : 'default user';
+console.log('username: ${username}' );
+
 
---
+++ Operator/4_logical_operators_continue.js
@@ -0,0 +1,13 @@
+// || operators return the first truthy value
+/**
+ * Falsy values in JS
+ * - false, 0, "", '', ``, null, undefined, NaN
+ */
+console.log(false || "Bronco" || "CPP"); // bronco
+console.log(false || 11 || 12); // 11
+
+
+//&& operators return the first falsy values
+console.log("Cal Poly" && 0); // 0
+console.log(1 && "CPP" && -10);
+
