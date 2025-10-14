4edfa87ef621f1cc24338b8c41e5f6c4f87dacc4
Cargoeschoochoo <150305988+Cargoeschoochoo@users.noreply.github.com>
2025-10-14 22:39:44 +0000

    changed 5_for_in_loops.js file

--- Control_flow/5_for_in_loops.js
+++ Control_flow/5_for_in_loops.js
@@ -4,8 +4,15 @@ const course = {
     'section': 3,
 }
 //For in loop requires [] no .name
+
 for(const key in course){
     console.log(course[key]);
 }
-
 console.log(course.name);
+console.log(course['name']);
+
+const numbers = [11, 12, 13, 14];
+for(const idx in numbers){
+    console.log(`${idx}. ${numbers[idx]}`);
+    
+}
